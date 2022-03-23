4d79a74a47828afd15c4baa231842ea819f6fa23
Akin Capar <akincapar2@gmail.com>
2022-03-23 12:59:27 +0300

    Complete game loop with minor excuses.

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -8,6 +8,7 @@ public class UI : MonoBehaviour
 {
     public static UI instance;
     public int sceneNo;
+    LevelManager levelManager;
 
     [SerializeField] public GameObject startCanvas;
     [SerializeField] public GameObject gameCanvas;
@@ -18,15 +19,23 @@ public class UI : MonoBehaviour
     [SerializeField] public GameObject gameCanvasLevelCompletionImage3;
     [SerializeField] public TextMeshProUGUI levelNoText;
     [SerializeField] public TextMeshProUGUI nextLevelNoText;
-    private int levelNo;
-    private int nextLevelNo;
+    [SerializeField] public TextMeshProUGUI collectedAmount1;
+    [SerializeField] public TextMeshProUGUI neededAmount1;
+    [SerializeField] public TextMeshProUGUI collectedAmount2;
+    [SerializeField] public TextMeshProUGUI neededAmount2;
+    [SerializeField] public TextMeshProUGUI collectedAmount3;
+    [SerializeField] public TextMeshProUGUI neededAmount3;
+    public int levelNo = 1;
+    public int nextLevelNo = 2;
 
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
         }
+
 
         Time.timeScale = 0;
         sceneNo = SceneManager.GetActiveScene().buildIndex;
@@ -34,10 +43,22 @@ public class UI : MonoBehaviour
 
     private void Start()
     {
+        levelManager = LevelManager.instance;
         levelNo = sceneNo + 1;
         nextLevelNo = levelNo + 1;
         levelNoText.text = levelNo.ToString();
         nextLevelNoText.text = nextLevelNo.ToString();
+    }
+
+    private void Update()
+    {
+        Debug.Log(levelManager.collectedObjectsAmount1);
+        collectedAmount1.text = levelManager.collectedObjectsAmount1.ToString();
+        neededAmount1.text = levelManager.neededObjectsAmount1.ToString();
+        collectedAmount2.text = levelManager.collectedObjectsAmount2.ToString();
+        neededAmount2.text = levelManager.neededObjectsAmount2.ToString();
+        collectedAmount3.text = levelManager.collectedObjectsAmount3.ToString();
+        neededAmount3.text = levelManager.neededObjectsAmount3.ToString();
     }
 
     public void StartGame()
@@ -60,7 +81,21 @@ public class UI : MonoBehaviour
 
     public void NextLevel()
     {
-        SceneManager.LoadScene(sceneNo + 1);
+        if (sceneNo == 9)
+        {
+
+            SceneManager.LoadScene(Random.Range(0, 9));
+            levelNo++;
+            nextLevelNo++;
+        }
+
+        else
+        {
+            SceneManager.LoadScene(sceneNo + 1);
+            levelNo++;
+            nextLevelNo++;
+
+        }
     }
 
     public void ActivateGameCanvas()
